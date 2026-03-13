17f59ff800d60a0657e530cb21e8a40683970cdb
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-03-12 23:12:45 -0500

    feat: Crear clase abstracta persona

--- src/sistemagestion/model/Persona.java
+++ src/sistemagestion/model/Persona.java
@@ -1,13 +1,81 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sistemagestion.model;
 
-/**
- *
- * @author Lenovo
- */
-public class Persona {
-    
+public abstract class Persona {
+
+    private String tipoDocumento;
+    private String documento;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String fechaNacimiento;
+
+    public Persona() {
+    }
+
+    public Persona(String tipoDocumento, String documento, String nombre, String apellido, String telefono, String fechaNacimiento) {
+        this.tipoDocumento = tipoDocumento;
+        this.documento = documento;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Tipo Documento: " + tipoDocumento
+                + "\nDocumento: " + documento
+                + "\nNombre: " + nombre
+                + "\nApellido: " + apellido
+                + "\nTelefono: " + telefono
+                + "\nFecha Nacimiento: " + fechaNacimiento;
+    }
 }

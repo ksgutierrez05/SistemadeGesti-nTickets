b8c943567e64fd462da25331f313115c4cfcd687
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-03-13 20:56:23 -0500

    feat: Crear clase Ticket con calculo de valor

---
+++ src/sistemagestion/model/Ticket.java
@@ -0,0 +1,68 @@
+package sistemagestion.model;
+
+public class Ticket {
+
+    private String codigo;
+    private Pasajero pasajero;
+    private double precioBase;
+    private double descuento;
+    private double valorFinal;
+
+    public Ticket() {
+    }
+
+    public Ticket(String codigo, Pasajero pasajero, double precioBase) {
+        this.codigo = codigo;
+        this.pasajero = pasajero;
+        this.precioBase = precioBase;
+    }
+
+    public void calcularValorFinal() {
+
+        descuento = pasajero.obtenerDescuento();
+
+        valorFinal = precioBase - (precioBase * descuento);
+
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public double getValorFinal() {
+        return valorFinal;
+    }
+
+    @Override
+    public String toString() {
+        return "Codigo Ticket: " + codigo
+                + "\nPasajero: " + pasajero.getNombre() + " " + pasajero.getApellido()
+                + "\nPrecio Base: " + precioBase
+                + "\nDescuento: " + descuento
+                + "\nValor Final: " + valorFinal;
+    }
+}

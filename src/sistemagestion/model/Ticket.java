8ab95c8691eba2f5c7663b612f3d71e84779c4a5
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-03-16 21:38:17 -0500

    feat: mejorar detalle de impresión del tickets y correcion de sintaxis

--- src/sistemagestion/model/Ticket.java
+++ src/sistemagestion/model/Ticket.java
@@ -70,22 +70,28 @@ public double calcularTotal(boolean esFestivo) {
     public String getCodigo() {
     return codigo;
 }
-    public void imprimirDetalle() {
-        System.out.println("----- TICKET -----");
-        System.out.println("Código: " + codigo);
-        System.out.println("Pasajero: " + pasajero.getNombre());
-        System.out.println("Fecha Compra: " + fechaCompra);
-        System.out.println("Precio Base: " + precioBase);
-        System.out.println("Descuento aplicado: " + (descuento*100) + "%");
-        System.out.println("Valor Final: " + valorFinal);
-        System.out.println("-----------------");
-    }
-
     @Override
-    public double calcularTotal() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+public double calcularTotal() {
+    return calcularTotal(false);
+}
+    
+public void imprimirDetalle() {
+        System.out.println("===== TICKET =====");
+        System.out.println("Código: " + codigo);
+        System.out.println("Fecha de Compra: " + fechaCompra);
+
+        System.out.println("\n--- PASAJERO ---");
+        pasajero.imprimirDetalle(); 
+
+        System.out.println("\n--- VEHÍCULO ---");
+        vehiculo.imprimirDetalle();  
+
+        System.out.println("\nPrecio Base: $" + precioBase);
+        System.out.println("Descuento aplicado: " + (descuento * 100) + "%");
+        System.out.println("Valor Final: $" + valorFinal);
+        System.out.println("=================\n");
     }
 
-
+    
 
 }

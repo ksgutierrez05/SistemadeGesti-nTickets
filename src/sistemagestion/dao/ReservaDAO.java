b250818dda439f7094b009ab4609bae2bb97479f
maria cristina martinez hinojosa <mariacristinamartinez@unicesar.edu.co>
2026-03-31 16:30:36 -0500

    refactor(reserva-dao): mueve consultas de reservas activas al dao y adapta persistencia con enum

--- src/sistemagestion/dao/ReservaDAO.java
+++ src/sistemagestion/dao/ReservaDAO.java
@@ -12,6 +12,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import sistemagestion.model.EstadoReserva;
 import sistemagestion.model.Reserva;
 
 /**
@@ -19,176 +20,201 @@ import sistemagestion.model.Reserva;
  * @author Maria Cristina
  */
 public class ReservaDAO {
+
     private File Archivos;
-    public ReservaDAO(){
+
+    public ReservaDAO() {
         Archivos = new File("Reservas.txt");
-         try {
+        try {
             if (!Archivos.exists()) {
                 Archivos.createNewFile();
-                System.out.println("Archivo creado");
+
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-}
-    public void guardarReserva(Reserva reserva){
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter(Archivos, true))) {
-
-        bw.write(
-            reserva.getCodigo() + ";" +
-            reserva.getDocumentoPasajero() + ";" +
-            reserva.getPlacaVehiculo() + ";" +
-            reserva.getFechaCreacion() + ";" +
-            reserva.getFechaViaje() + ";" +
-            reserva.getEstado()
-        );
-
-        bw.newLine();
-
-    } catch (IOException e) {
-        e.printStackTrace();
     }
-}
-    
-    public List<Reserva> listarReservas(){
-    List<Reserva> lista = new ArrayList<>();
 
-    try (BufferedReader br = new BufferedReader(new FileReader(Archivos))) {
-        String linea;
+    public void guardarReserva(Reserva reserva) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(Archivos, true))) {
 
-        while ((linea = br.readLine()) != null) {
-            String[] datos = linea.split(";");
+            bw.write(
+                    reserva.getCodigo() + ";"
+                    + reserva.getDocumentoPasajero() + ";"
+                    + reserva.getPlacaVehiculo() + ";"
+                    + reserva.getFechaCreacion() + ";"
+                    + reserva.getFechaViaje() + ";"
+                    + reserva.getEstado().name()
+            );
 
-            if(datos.length == 6){
-                Reserva r = new Reserva(
-                    datos[0],
-                    datos[1],
-                    datos[2],
-                    datos[3],
-                    datos[4],
-                    datos[5]
-                );
+            bw.newLine();
 
-                lista.add(r);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Reserva> listarReservas() {
+        List<Reserva> lista = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(Archivos))) {
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(";");
+
+                if (datos.length == 6) {
+                    Reserva r = new Reserva(
+                            datos[0],
+                            datos[1],
+                            datos[2],
+                            datos[3],
+                            datos[4],
+                            EstadoReserva.valueOf(datos[5].toUpperCase())
+                    );
+
+                    lista.add(r);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
+
+    public String buscarReserva(String codigo) {
+        try (BufferedReader br = new BufferedReader(new FileReader(Archivos))) {
+
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+
+                String[] datos = linea.split(";");
+
+                if (datos[0].equals(codigo)) {
+                    return linea;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void actualizarReserva(Reserva reserva) {
+        File temp = new File("temp.txt");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(Archivos)); FileWriter fw = new FileWriter(temp)) {
+
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+
+                String[] datos = linea.split(";");
+
+                if (datos[0].equals(reserva.getCodigo())) {
+
+                    fw.write(
+                            reserva.getCodigo() + ";"
+                            + reserva.getDocumentoPasajero() + ";"
+                            + reserva.getPlacaVehiculo() + ";"
+                            + reserva.getFechaCreacion() + ";"
+                            + reserva.getFechaViaje() + ";"
+                            + reserva.getEstado().name() + "\n"
+                    );
+
+                } else {
+                    fw.write(linea + "\n");
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Archivos.delete();
+        temp.renameTo(Archivos);
+    }
+
+    public void eliminarReserva(String codigo) {
+        File temp = new File("temp.txt");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(Archivos)); FileWriter fw = new FileWriter(temp)) {
+
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+
+                String[] datos = linea.split(";");
+
+                if (!datos[0].equals(codigo)) {
+                    fw.write(linea + "\n");
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Archivos.delete();
+        temp.renameTo(Archivos);
+    }
+
+    public Reserva buscarReservaPorCodigo(String codigo) {
+        try (BufferedReader br = new BufferedReader(new FileReader(Archivos))) {
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(";");
+
+                if (datos.length == 6 && datos[0].equalsIgnoreCase(codigo)) {
+                    return new Reserva(
+                        datos[0],
+                        datos[1],
+                        datos[2],
+                        datos[3],
+                        datos[4],
+                        EstadoReserva.valueOf(datos[5].toUpperCase())
+                    );
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    public boolean existeReservaActivaPasajeroVehiculoFecha(String documentoPasajero, String placaVehiculo, String fechaViaje) {
+        for (Reserva r : listarReservas()) {
+            if (r.getDocumentoPasajero().equalsIgnoreCase(documentoPasajero)
+                    && r.getPlacaVehiculo().equalsIgnoreCase(placaVehiculo)
+                    && r.getFechaViaje().equals(fechaViaje)
+                    && r.getEstado() == EstadoReserva.ACTIVA) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public int contarReservasActivasPorVehiculoYFecha(String placaVehiculo, String fechaViaje) {
+        int contador = 0;
+
+        for (Reserva r : listarReservas()) {
+            if (r.getPlacaVehiculo().equalsIgnoreCase(placaVehiculo)
+                    && r.getFechaViaje().equals(fechaViaje)
+                    && r.getEstado() == EstadoReserva.ACTIVA) {
+                contador++;
             }
         }
 
-    } catch (IOException e) {
-        e.printStackTrace();
+        return contador;
     }
 
-    return lista;
-}
-    
-    public String buscarReserva(String codigo){
-    try (BufferedReader br = new BufferedReader(new FileReader(Archivos))) {
 
-        String linea;
 
-        while ((linea = br.readLine()) != null) {
 
-            String[] datos = linea.split(";");
-
-            if (datos[0].equals(codigo)) {
-                return linea;
-            }
-        }
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-    return null;
-}
-    public void actualizarReserva(Reserva reserva){
-    File temp = new File("temp.txt");
-
-    try (BufferedReader br = new BufferedReader(new FileReader(Archivos));
-         FileWriter fw = new FileWriter(temp)) {
-
-        String linea;
-
-        while ((linea = br.readLine()) != null) {
-
-            String[] datos = linea.split(";");
-
-            if (datos[0].equals(reserva.getCodigo())) {
-
-                fw.write(
-                    reserva.getCodigo() + ";" +
-                    reserva.getDocumentoPasajero() + ";" +
-                    reserva.getPlacaVehiculo() + ";" +
-                    reserva.getFechaCreacion() + ";" +
-                    reserva.getFechaViaje() + ";" +
-                    reserva.getEstado() + "\n"
-                );
-
-            } else {
-                fw.write(linea + "\n");
-            }
-        }
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-    Archivos.delete();
-    temp.renameTo(Archivos);
-}
-    public void eliminarReserva(String codigo){
-    File temp = new File("temp.txt");
-
-    try (BufferedReader br = new BufferedReader(new FileReader(Archivos));
-         FileWriter fw = new FileWriter(temp)) {
-
-        String linea;
-
-        while ((linea = br.readLine()) != null) {
-
-            String[] datos = linea.split(";");
-
-            if (!datos[0].equals(codigo)) {
-                fw.write(linea + "\n");
-            }
-        }
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-    Archivos.delete();
-    temp.renameTo(Archivos);
-}
-
-public Reserva buscarReservaPorCodigo(String codigo) {
-    try (BufferedReader br = new BufferedReader(new FileReader(Archivos))) {
-
-        String linea;
-
-        while ((linea = br.readLine()) != null) {
-
-            String[] datos = linea.split(";");
-
-            if (datos.length == 6 && datos[0].equals(codigo)) {
-
-                return new Reserva(
-                        datos[0], // codigo
-                        datos[1], // documento
-                        datos[2], // placa
-                        datos[3], // fecha creacion
-                        datos[4], // fecha viaje
-                        datos[5]  // estado
-                );
-            }
-        }
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-    return null;
-}
-    
-    
-    
 }
